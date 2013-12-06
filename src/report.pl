03b0f460ef2d1e2f128fed55d40834871b78d715
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-12-06 13:52:14 +0100

    Implements reportAskUser/2

--- src/report.pl
+++ src/report.pl
@@ -1,6 +1,8 @@
 
 :- [checkboxes].
 
+convert_str_char([C|_],C).
+
 %
 % Creates a new empty report
 %
@@ -40,4 +42,29 @@ reportPrint(Report,[Name|L]) :-
     ),
     write('\n'),
     reportPrint(Report,L).
+
+%
+% Generates a repport asking question to the users
+%
+reportAskUser(Report,UserName) :-
+    findall(X,checkboxTitle(X,_),AllNames),
+    reportAskUser(Report,UserName,AllNames).
+
+reportAskUser(Report,_,[]) :-
+    reportCreate(Report).
+
+reportAskUser(Report,UserName,[Name|NamesRemaining]) :-
+    checkboxTitle(Name,Title),
+    write(Title),
+    write(': Oui (o) ou Non (n) ?\n'),
+    write(UserName),
+    write(' > '),
+    get_single_char(C),
+    write('\n'),
+    reportAskUser(ReturnedReport,UserName,NamesRemaining),
+    ( convert_str_char("o",C) ->
+        reportCheck(ReturnedReport,Name,Report)
+    ); (
+        Report = ReturnedReport
+    ).
 
