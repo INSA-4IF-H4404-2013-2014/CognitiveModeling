417d6de519d8ecc9781c57a13fda31114554cee5
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-12-06 12:48:00 +0100

    Lists all checkboxes in src/checkboxes.pl

---
+++ src/checkboxes.pl
@@ -0,0 +1,9 @@
+
+#
+# defines all checkboxes' titles
+#
+checkboxTitle(c00, "en stationnement / a l'arret").
+checkboxTitle(c01, "quittait un stationnement / ouvrait une portiere").
+
+checkboxTitle(c00, "en stationnement / a l'arret").
+
