2c8484d08fecac358ff785edde7cb2bafec11eba
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-12-07 16:02:26 +0100

    Implements reportEvaluateWrongs/4

--- src/reportEvaluateWrongs.pl
+++ src/reportEvaluateWrongs.pl
@@ -1,28 +1,70 @@
 
 :- [report].
 
-%
-% Evaluates wrong between to cars' repport
-%
-reportEvaluateFatalMistake(ReportA,ReportB,100) :-
-    reportHasFatalMistake(ReportA),
-    not(reportHasFatalMistake(ReportB)).
-
-reportEvaluateFatalMistake(ReportA,ReportB,0) :-
-    not(reportHasFatalMistake(ReportA)),
-    reportHasFatalMistake(ReportB).
-
-reportEvaluateFatalMistake(ReportA,ReportB,50) :-
-    reportHasFatalMistake(ReportA),
-    reportHasFatalMistake(ReportB).
 
 %
-% Evaluates if report has done a fatal mistake
+% Compute the other car wrongs
 %
-reportHasFatalMistake(Report) :-
-    (
-        reportIsChecked(Report,c04);
-        reportIsChecked(Report,c07);
-        reportIsChecked(Report,c14)
-    ).
+reportSymetricWrongs(WrongsA,WrongsB) :-
+    WrongsB is 100 - WrongsA.
+
+%
+% Lists all rules in a predicate
+%
+:- dynamic reportEvaluateRules/1.
+:- retractall(reportEvaluateRules(_)).
+
+%
+% Defines a new rule
+%
+reportDefineRule(RulePredicate) :-
+    assert(reportEvaluateRules(RulePredicate)).
+
+%
+% Fetches all new rules
+%
+reportListRules(Rules) :-
+    findall(X,reportEvaluateRules(X),Rules).
+
+%
+% Evaluate wrongs between to cars
+%
+reportEvaluateWrongs(ReportA,ReportB,WrongsA,Evaluator) :-
+    reportListRules(Rules),
+    reportEvaluateWrongs(ReportA,ReportB,WrongsA,Evaluator,Rules).
+
+reportEvaluateWrongs(_,_,_,_,[]) :- fail.
+reportEvaluateWrongs(ReportA,ReportB,WrongsAReturned,Evaluator,[Rule|Rules]) :-
+    call(Rule,ReportA,ReportB,WrongsA) -> (
+        Evaluator = Rule,
+        call(Rule,ReportB,ReportA,WrongsA) -> (
+            WrongsAReturned = 50
+        ); (
+            WrongsAReturned = WrongsA
+        )
+    );
+    call(Rule,ReportB,ReportA,WrongsB) -> (
+        reportSymetricWrongs(WrongsAReturned,WrongsB),
+        Evaluator = Rule
+    );
+    reportEvaluateWrongs(ReportA,ReportB,WrongsAReturned,Evaluator,Rules).
+
+%
+% Evaluates fata mistake between to cars' repport
+%
+reportEvaluateFatalMistake(A,_,100) :-
+    reportIsChecked(A,c04);
+    reportIsChecked(A,c07);
+    reportIsChecked(A,c14).
+
+:- reportDefineRule(reportEvaluateFatalMistake).
+
+%
+% Rules 1
+%
+reportEvaluate1(A,_,100) :-
+    reportIsChecked(A,c01),
+    reportIsChecked(A,c02).
+
+:- reportDefineRule(reportEvaluate1).
 
--- src/testReportEvaluateWrongs.pl
+++ src/testReportEvaluateWrongs.pl
@@ -5,10 +5,23 @@
 testReportEvaluateFatalMistake :-
     reportCreate(G0),
     reportCheck(G0,c04,G1),
-    reportEvaluateFatalMistake(G1,G0,100),
-    reportEvaluateFatalMistake(G0,G1,0),
-    reportEvaluateFatalMistake(G1,G1,50).
+    reportEvaluateFatalMistake(G1,G0,100).
+
+testReportEvaluateWrongsMechanism :-
+    reportCreate(G0),
+    reportCheck(G0,c04,G1),
+    reportCheck(G0,c01,G2),
+    reportCheck(G2,c02,G3),
+    reportEvaluateWrongs(G0,G1,0,_),
+    reportEvaluateWrongs(G1,G0,100,_),
+    reportEvaluateWrongs(G1,G1,50,_),
+    reportEvaluateWrongs(G0,G3,0,_),
+    reportEvaluateWrongs(G3,G0,100,_),
+    reportEvaluateWrongs(G3,G3,50,_),
+    reportEvaluateWrongs(G1,G3,100,_),
+    reportEvaluateWrongs(G3,G1,0,_).
 
 testReportEvaluateWrongs :-
-    test(testReportEvaluateFatalMistake).
+    test(testReportEvaluateFatalMistake),
+    test(testReportEvaluateWrongsMechanism).
 
