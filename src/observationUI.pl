d4a44aab01f033d11e387fdb46f578b793336047
Aline Martin <aline.martin@insa-lyon.fr>
2013-12-10 09:49:51 +0100

    the programm can ask a line from the user and display the list of the rules

--- src/observationUI.pl
+++ src/observationUI.pl
@@ -2,10 +2,35 @@
 
 :- [checkboxes].
 
+checkboxesLinePrint([]).
+checkboxesLinePrint([H|List]):- 
+    checkboxTitle(H, Title), 
+    write(H),
+    write(' '),
+    write(Title),
+    nl,
+    checkboxesLinePrint(List).
+
+checkboxesPrint:- 
+    nl,
+    checkboxesList(AllNames),
+    checkboxesLinePrint(AllNames),
+    nl.
+
+
 %
 % get/1 get a line of text
+% read/1 idem
 %
 
-testGet :- 
-    get(Text),
-    write(Text).
+textPrint(X) :- 
+    atom_codes(C,X),
+    write(C).
+
+testRead :- 
+    checkboxesPrint,
+    read(X),
+    nl,
+    textPrint(X).
+
+
