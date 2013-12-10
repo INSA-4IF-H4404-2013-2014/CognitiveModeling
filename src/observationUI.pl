f0d83ed0f8a42752fe37f182b35acab1a1ba74f0
Aline Martin <aline.martin@insa-lyon.fr>
2013-12-10 20:07:36 +0100

    improve the interface, now it check the case the user choosed

--- src/observationUI.pl
+++ src/observationUI.pl
@@ -1,14 +1,14 @@
 
 
-:- [checkboxes].
+:-[checkboxes].
+:-[reportRules].
 
 checkboxesLinePrint([]).
 checkboxesLinePrint([H|List]):- 
     checkboxTitle(H, Title), 
     write(H),
     write(' '),
-    write(Title),
-    nl,
+    write(Title), nl,
     checkboxesLinePrint(List).
 
 checkboxesPrint:- 
@@ -17,7 +17,6 @@ checkboxesPrint:-
     checkboxesLinePrint(AllNames),
     nl.
 
-
 %
 % get/1 get a line of text
 % read/1 idem
@@ -25,22 +24,45 @@ checkboxesPrint:-
 % split_string(+String, +SepChars, +PadChars, -SubStrings)
 
 writeList([]).
-writeList([T|Q]) :- 
-    write(T),
-    nl,
+writeList([T|Q]) :-
+    write(T), nl,
     writeList(Q).
 
+
+checkList(New, [], New).
+checkList(Col, [T|Q], New) :- 
+    reportCheck(Col, T, Inter),
+    checkList(Inter, Q, New).
 
 splitText(X,L) :- 
     atomic_list_concat(L,' ', X).
 
-
-testRead :- 
+userRead(Col, Letter, Cond) :- 
     checkboxesPrint,
-    read(X),
-    nl,
+    write('conducteur '),
+    write(Letter),
+    write(' cases ? :'), nl,
+    read(X), nl,
     atom_codes(C,X),
     splitText(C,L),
-    writeList(L).
+    checkList(Col, L, Cond).
+
+testCheckList:-
+    L = ['c01', 'c15'],
+    reportCreate(G0),
+    checkList(G0, L, R),
+    write(R).
+    
+
+makeReport :-
+    reportCreate(G0),
+    userRead(G0, 'A', A),
+    writeList(A),
+    userRead(G0, 'B', B),
+    writeList(B).
+
+
+    
+
 
 
